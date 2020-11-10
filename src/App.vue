1e1ed6ccf5e4ab0468fb11ef0e36f2043ccf8db8
Edwin Rebolledo <wedwin53@gmail.com>
2020-11-09 19:04:11 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <img
+      alt="Yuliana Coach Logo"
+      class="logo"
+      src="./assets/yc-logo-tiny.jpg"
+    />
+    <HelloWorld msg="Bienvenid@" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;500;700&display=swap");
+#app {
+  font-family: "Ubuntu", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+html {
+  background-image: url("dominic-dreier-kztP2WFVIXM-unsplash.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100%;
+}
+
+.logo {
+  max-width: 60%;
+  box-shadow: rgb(105, 101, 101) 5px 10px 15px;
+  border-radius: 10px;
+}
+
+@media (min-width: 600px) {
+  .logo {
+    max-width: 25%;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1 class="title-text">{{ msg }}</h1>
+
+    <p class="welcome-text">
+      Nos encontramos creando un producto maravilloso para mejorar tu calidad de
+      vida!
+    </p>
+    <br />
+    <p class="text-newsletter">
+      Suscribase a nuestra lista para recursos y lanzamientos
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  margin-top: 10px;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: rgba(255, 255, 255, 0.7);
+  box-shadow: rgba(29, 22, 22, 0.5) 4px 2px 2px;
+}
+
+h1 {
+  margin: 40px 0 0;
+  font-size: 40px;
+  color: black;
+  text-shadow: rgba(77, 76, 74, 0.5) 10px 10px 10px;
+}
+
+.welcome-text {
+  font-size: 25px;
+  font-weight: 300;
+  color: black;
+}
+
+.text-newsletter {
+  color: black;
+  font-size: 25px;
+  font-weight: 300;
+}
+</style>
